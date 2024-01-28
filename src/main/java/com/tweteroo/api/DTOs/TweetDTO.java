1f4cbef00c332bf657c8503a28d056366ac665e3
Nicolas Araújo <nicolasaraujo@gmail.com>
2024-01-28 18:35:32 -0300

    feat: DTOs user and tweet

--- src/main/java/com/tweteroo/api/DTOs/TweetDTO.java
+++ src/main/java/com/tweteroo/api/DTOs/TweetDTO.java
@@ -1,6 +1,17 @@
 package com.tweteroo.api.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class TweetDTO {
 
+    @NotBlank
+    @Size(min = 4, max = 280, message = "The tweet must be no longer than 280 characters and no shorter than 4 characters")
+    private String text;
+
+    @Positive
+    private Long userId;
 }
--- src/main/java/com/tweteroo/api/DTOs/UserDTO.java
+++ src/main/java/com/tweteroo/api/DTOs/UserDTO.java
@@ -1,6 +1,14 @@
 package com.tweteroo.api.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class UserDTO {
 
+    @NotBlank
+    private String avatar;
+
+    @NotBlank
+    @Size(max = 100, min = 3, message = "The name must be greater than 4 characters and less than 100 characters")
+    private String username;
 }
