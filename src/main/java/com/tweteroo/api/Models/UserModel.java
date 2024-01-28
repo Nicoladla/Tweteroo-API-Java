d7f9690665c6c8fd42be951233ade83b851f44c6
Nicolas Araújo <nicolasaraujo@gmail.com>
2024-01-28 18:17:48 -0300

    feat: models user and tweet

--- src/main/java/com/tweteroo/api/Models/UserModel.java
+++ src/main/java/com/tweteroo/api/Models/UserModel.java
@@ -1,5 +1,29 @@
 package com.tweteroo.api.Models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class UserModel {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String avatar;
+
+    @Column(nullable = false, unique= true, length = 100)
+    private String username;
 }
