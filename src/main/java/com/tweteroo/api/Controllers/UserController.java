e0619197e4b7a04323897ab57dbbeea3a8fdba87
Nicolas Araújo <nicolasaraujo@gmail.com>
2024-01-28 19:21:08 -0300

    feat: find list users

--- src/main/java/com/tweteroo/api/Controllers/UserController.java
+++ src/main/java/com/tweteroo/api/Controllers/UserController.java
@@ -1,5 +1,42 @@
 package com.tweteroo.api.Controllers;
 
-public class UserController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.tweteroo.api.DTOs.UserDTO;
+import com.tweteroo.api.Models.UserModel;
+import com.tweteroo.api.Services.UserService;
+
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    final UserService userService;
+
+    public UserController (UserService userService){
+        this.userService= userService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UserModel>> getUsers() {
+        List<UserModel> users= userService.findUsers();
+
+        return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    @PostMapping()
+    public void postUser(@RequestBody @Valid UserDTO body){
+
+    }
 }
--- src/main/java/com/tweteroo/api/Services/UserService.java
+++ src/main/java/com/tweteroo/api/Services/UserService.java
@@ -1,5 +1,23 @@
 package com.tweteroo.api.Services;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tweteroo.api.Models.UserModel;
+import com.tweteroo.api.Repositories.UserRepository;
+
+@Service
 public class UserService {
+
+    final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository= userRepository;
+    }
     
+    public List<UserModel> findUsers(){
+        return userRepository.findAll();
+    }
+
 }
