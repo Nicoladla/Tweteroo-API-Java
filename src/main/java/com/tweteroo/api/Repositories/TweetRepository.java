06078d7fc0097859fdd77e57addd6257508f073b
Nicolas Araújo <nicolasaraujo@gmail.com>
2024-01-29 12:42:48 -0300

    feat: tweet repository

--- src/main/java/com/tweteroo/api/Repositories/TweetRepository.java
+++ src/main/java/com/tweteroo/api/Repositories/TweetRepository.java
@@ -1,8 +1,11 @@
 package com.tweteroo.api.Repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.tweteroo.api.Models.TweetModel;
+
 @Repository
-public class TweetRepository {
-    
+public interface TweetRepository extends JpaRepository<TweetModel, Long> {
+
 }
