48c9c3444fefb190a32e8266d0753c2f8496076f
Nicolas Araújo <nicolasaraujo@gmail.com>
2024-01-29 12:59:08 -0300

    feat: route for the user to be able to get the list of tweets

--- src/main/java/com/tweteroo/api/Controllers/TweetController.java
+++ src/main/java/com/tweteroo/api/Controllers/TweetController.java
@@ -3,8 +3,30 @@ package com.tweteroo.api.Controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tweteroo.api.Models.TweetModel;
+import com.tweteroo.api.Services.TweetService;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @RestController
 @RequestMapping("/tweets")
 public class TweetController {
-    
+
+    final TweetService tweetService;
+
+    public TweetController(TweetService tweetService) {
+        this.tweetService = tweetService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<TweetModel>> getTweets(String param) {
+        List<TweetModel> tweets = tweetService.findTweets();
+
+        return ResponseEntity.status(HttpStatus.OK).body(tweets);
+    }
+
 }
--- src/main/java/com/tweteroo/api/Services/TweetService.java
+++ src/main/java/com/tweteroo/api/Services/TweetService.java
@@ -1,8 +1,21 @@
 package com.tweteroo.api.Services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.tweteroo.api.Models.TweetModel;
+import com.tweteroo.api.Repositories.TweetRepository;
 
 @Service
 public class TweetService {
-    
+    final TweetRepository tweetRepository;
+
+    public TweetService(TweetRepository tweetRepository) {
+        this.tweetRepository = tweetRepository;
+    }
+
+    public List<TweetModel> findTweets() {
+        return tweetRepository.findAll();
+    }
 }
