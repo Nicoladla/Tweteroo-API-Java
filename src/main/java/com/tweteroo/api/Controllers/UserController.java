b8af4a966b6e6e9dbb1b124434c4656f0acf1eea
Nicolas Araújo <nicolasaraujo@gmail.com>
2024-01-29 20:59:49 -0300

    feat: cors

--- src/main/java/com/tweteroo/api/Controllers/UserController.java
+++ src/main/java/com/tweteroo/api/Controllers/UserController.java
@@ -14,10 +14,12 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
